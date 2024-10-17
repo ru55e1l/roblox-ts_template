697e1a3ee5e0b5975b282656ad9cf3e0c44d7c2b
Russell Toney <91279889+ru55e1l@users.noreply.github.com>
2024-10-17 01:49:28 -0400

    Create IController.ts

---
+++ src/hyperdrive/client/controllers/IController.ts
@@ -0,0 +1,11 @@
+/*
+HYPERDRIVE
+-------------------------------------------------------------------------------------------------->
+Description:
+
+Interface for controllers (Player and Character scripts)
+-------------------------------------------------------------------------------------------------->*/
+
+interface IController {}
+
+export { IController };
